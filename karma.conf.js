a419f52de14c50e16d5a70ffdaa4ffb0489852ce
Emjay <abdulmujeeb.jamiu@andela.com>
2017-02-13 20:58:48 +0100

    fix: try chrome

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers:  ['Chrome'],
+    browsers: process.env.TRAVIS ? ['Chrome_travis_ci'] : ['Chrome'],
 
 
     // Continuous Integration mode
